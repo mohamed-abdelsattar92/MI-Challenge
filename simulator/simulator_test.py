0157c051c3585e86eacbdef8fba54c20d77ad0f1
Mohamed Abdel Sattar <mohamed.abdelsattar@giscon.de>
2019-08-17 22:36:13 +0200

    Remove the simulator from the old location

--- simulator/simulator_test.py
+++
@@ -1,9 +0,0 @@
-from simulator import Simulator
-
-BERLING_BOUNDING_BOX = (13.091992716067702, 52.33488609760638, 13.742786470433, 52.67626223889507)
-
-
-sim = Simulator(BERLING_BOUNDING_BOX)
-result = sim.simulate(100)
-
-print(result['booking_distance_bins'])
